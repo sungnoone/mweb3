f5239965c2d90e37c754e6ceb3130568ac376c65
sungnoone <sungnoone@gmail.com>
2014-12-16 08:31:15 +0800

    v0.01
    cn checker project

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by hl on 2014/12/11.
+ */
+
+var RESTFUL_HOST = "http://192.168.1.229:8088"; //服務主機
+var RESTFUL_URL_TEST = "/msrv3/"; //測試用
+
+
+function btn_test(){
+    var url = RESTFUL_HOST;
+    var data = "Sending data";
+    $.ajax({
+        type: "GET",
+        url:url,
+        contentType: "text/plain; charset=utf-8",
+        dataType: "text",
+        success: function(data, status, jqXHR){
+            alert(data);
+        },
+        error: function(jqXHR, status){
+            alert("FAIL!");
+        }
+    });
+}
