24156cfc17ad09bc6331fe087e186c4d926dba7b
sungnoone <sungnoone@gmail.com>
2015-02-02 09:56:32 +0800

    v0.06

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hl on 2014/12/11.
+ */
+
+var RESTFUL_HOST = "http://192.168.1.229:8088"; //服務主機
+var RESTFUL_SRV_QUERY_CN_WORDS_ALL = "/srv/cn/get/all/"; //服務主機
+var RESTFUL_SRV_QUERY_CN_WORDS_TYPE1 = "/srv/cn/get/type1/";
+
